c1cbfb3af19eb38bf6d878d88c54ed471309a3f5
shafiqullah <shohagbit0404@gmail.com>
2024-09-29 19:44:05 -0700

    Another solution 104
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

--- Leetcode_105_max_depth_binary_tree.swift
+++ Leetcode_105_max_depth_binary_tree.swift
@@ -10,10 +10,25 @@ class Solution {
         guard let root = root else {
             return 0
         }
-
+        
         let left = maxDepth(root.left)
         let right = maxDepth(root.right)
         
         return max(left, right) + 1
     }
+    
+    //    or
+    //    func maxDepth(_ root: TreeNode?) -> Int {
+    //        return findMaxDepth(root, 0)
+    //    }
+    
+    func findMaxDepth(_ root: TreeNode?, _ count: Int)->Int{
+        guard let _ = root else{
+            return count
+        }
+        
+        return max(findMaxDepth(root?.left, count + 1), findMaxDepth(root?.right, count + 1))
+    }
 }
+
+
