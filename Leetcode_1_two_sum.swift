202cde4596b09733b51b7641c553d958ae875482
shafiqullah <shohagbit0404@gmail.com>
2024-10-02 11:34:56 -0700

    1 Two sum usingtwo pointer
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

--- Leetcode_1_two_sum.swift
+++ Leetcode_1_two_sum.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Solution {
+class Solution_Using_Hash {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         
         var hashMap:[Int: Int] = [:]
@@ -22,5 +22,41 @@ class Solution {
             hashMap[num] = index
         }
         return []
+    }
+}
+
+class Solution_Using_two_pointer {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        let forResult = nums
+        let nums = nums.sorted()
+        var left = 0
+        var rifht = nums.count - 1
+        var first_item: Int = -1
+        var second_item : Int = -1
+        var result : [Int] = []
+        
+        while left < rifht{
+            if target == nums[left] + nums[rifht]{
+                first_item = nums[left]
+                second_item = nums[rifht]
+                break
+            }else if target > nums[left] + nums[rifht]{
+                left += 1
+            }else{
+                rifht -= 1
+            }
+        }
+        
+        if first_item == -1 || second_item == -1{
+            return []
+        }
+        
+        for (index, n) in forResult.enumerated(){
+            if n == first_item || n == second_item {
+                result.append(index)
+            }
+        }
+        
+        return result
     }
 }
