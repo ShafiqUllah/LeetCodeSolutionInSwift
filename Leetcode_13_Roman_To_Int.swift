62fbcd5d7707e3873a258d951f0ccf2162fb899d
shafiqullah <shohagbit0404@gmail.com>
2024-10-03 18:07:15 -0700

    13 Roman to Interger
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

---
+++ Leetcode_13_Roman_To_Int.swift
@@ -0,0 +1,34 @@
+class Solution_13 {
+    func romanToInt(_ s: String) -> Int {
+        var hashTable: [Character:Int] = [:]
+        
+        hashTable["I"] = 1
+        hashTable["V"] = 5
+        hashTable["X"] = 10
+        hashTable["L"] = 50
+        hashTable["C"] = 100
+        hashTable["D"] = 500
+        hashTable["M"] = 1000
+        
+        var current = -1
+        var previous = -1
+        var sum = 0
+        
+        for c in s.reversed(){
+            if let val = hashTable[c]{
+                current = val
+                
+                if(previous <= current){
+                    sum += val
+                }else{
+                    sum -= val
+                }
+                previous = val
+                
+            }
+        }
+        return sum
+    }
+    
+    
+}
