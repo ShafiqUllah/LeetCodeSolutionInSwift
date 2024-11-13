fc2ac6e625aacc4d08827532395d5058241892bd
shafiqullah <shohagbit0404@gmail.com>
2024-11-12 23:14:59 -0800

    Binary search , find index or nearest index
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

---
+++ Binary Search.swift
@@ -0,0 +1,37 @@
+//this will return left most index
+func binarySearchLeft(_ nums: [Int], _ left: Int, _ right: Int, _ target: Int) -> Int {
+    var left = left
+    var right = right
+    var result = -1
+    
+    while left <= right {
+        let mid = left + (right - left) / 2
+        if nums[mid] >= target {
+            result = mid
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return result
+}
+
+//This will return right most index
+private func binarySearchRight(_ nums: [Int], _ left: Int, _ right: Int, _ target: Int) -> Int {
+    var left = left
+    var right = right
+    var result = -1
+    
+    while left <= right {
+        let mid = left + (right - left) / 2
+        if nums[mid] <= target {
+            result = mid
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return result
+}
+
+binarySearchLeft([1,2,3,4,5,6,7], 1, 6, 8)
